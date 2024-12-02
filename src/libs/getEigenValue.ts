d496b05fa9ebe853f37ed6fa6c1ebfa28bd9bc54
Nizhar Maulana <maulananizhar27@gmail.com>
2024-12-02 17:26:40 +0700

    feat: create getEigenValue function

---
+++ src/libs/getEigenValue.ts
@@ -0,0 +1,26 @@
+// function getValue(data: (string | number)[][]): number[] {
+//   // Mengambil indeks ke-1 dari setiap sub-array
+//   const result = data.map(subArray => Number(subArray[1]));
+//   // Menghilangkan elemen indeks ke-0 dari hasil
+//   return result.slice(1, -1);
+// }
+
+import { KriteriaType } from "@/types/kriteria";
+
+export function getEigenValue(
+  attributes: KriteriaType[],
+  data: (string | number)[][]
+): { [kriteria: string]: number } {
+  function getValue(data: (string | number)[][]): number[] {
+    // Mengambil indeks ke-1 dari setiap sub-array
+    const result = data.map(subArray => Number(subArray[1]));
+    // Menghilangkan elemen indeks ke-0 dari hasil
+    return result.slice(1, -1);
+  }
+
+  const value = getValue(data);
+  // Menggunakan reduce untuk membuat objek pasangan
+  return Object.fromEntries(
+    attributes.map((attr, index) => [attr.name.toLowerCase(), value[index]])
+  );
+}
